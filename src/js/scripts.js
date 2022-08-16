62a1a6a59736b08c7f0af86057911befd73aea58
mizza-cz <mizza.cz666@gmail.com>
2022-08-16 09:09:20 +0200

    add gallery

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,6 +3,8 @@
 //=include ../../node_modules/select2/dist/js/select2.min.js
 //=include ../../node_modules/photoswipe/dist/photoswipe.js
 //=include ../../node_modules/photoswipe/dist/photoswipe-ui-default.js
+//=include ../../node_modules/lightgallery/lightgallery.umd.js
+//=include ../../node_modules/lightgallery/lightgallery.min.js
 document.addEventListener("DOMContentLoaded", function() {
 //=include ../components/**/*.js
 //=include custom/*.js
